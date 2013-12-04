4272a2a8fe0e8edfcd782f20d5e2b1fcb6537921
Víðir Orri Reynisson <vidirr@gmail.com>
2013-12-04 13:33:10 +0000

    Added comments to season.cpp

--- verkefni6/season.cpp
+++ verkefni6/season.cpp
@@ -22,7 +22,14 @@ int main()
 
 string get_season(int month) {
 
-
+	/*
+		Notice how we combine cases that have the same value.
+		We have to specify what we want for each value that is correct (if we don't it goes to default.),
+		but we can combine cases 3, 4 and 5 since if we get case 3 we'll "slide down" to the return statement and then break,
+		so cases 3, 4, and 5 all have the same return value when we do it like this.
+		We could also have added returns and breaks for each statement, but this saves us some time, and we're lazy in general,
+		so we don't want to do anything we don't need to do.
+	*/
 	switch(month) {
 		case 1:
 		case 2:
